c1160708a2fec97e937b5c2cecf524fe59056938
Stephans-1 <kukustephansneriskwei@gmail.com>
2025-09-24 15:34:20 +0000

    Counter

--- src/app/components/counter/counter.ts
+++ src/app/components/counter/counter.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './counter.scss'
 })
 export class Counter {
+  counterValue = signal(0);
 
+  increment() {
+    this.counterValue.set(this.counterValue() + 1);
+  }
+  decrement() {
+    this.counterValue.set(this.counterValue() - 1);
+  }
+  reset() {
+    this.counterValue.set(0);
+  }
 }
